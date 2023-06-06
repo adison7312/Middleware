804fb140289eaf9a757eaa8b82b8da68779fff3f
Adrian <Adrian@LAPTOP-1UE0O909>
2023-06-06 16:12:19 +0200

    Dodaj pliki projektów.

---
+++ Middlewares/BrowserRedirectMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using UAParser;
+
+namespace Middleware.Middlewares
+{
+    public class BrowserRedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrowserRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var userAgent = context.Request.Headers["User-Agent"].ToString();
+            var parser = Parser.GetDefault();
+            var clientInfo = parser.Parse(userAgent);
+
+            var browserName = clientInfo.UA.Family.ToLower();
+
+            if (browserName == "edge" || browserName == "edgechromium" || browserName == "ie")
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class BrowserRedirectExtensions
+    {
+        public static IApplicationBuilder UseBrowserRedirect(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserRedirectMiddleware>();
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Middleware.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string UserAgent { get; private set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            UserAgent = Request.Headers["User-Agent"].ToString();
+        }
+    }
+}
